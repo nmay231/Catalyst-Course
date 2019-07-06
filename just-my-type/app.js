810e1e79e609a8eea39fe1806ce3d9df73018746
Noah May <noahmouse2011@gmail.com>
2019-07-05 20:14:46 -0600

    Cleaned up Just My Type

--- just-my-type/app.js
+++ just-my-type/app.js
@@ -1,63 +1,19 @@
+const SPACE = 'space'
+const NEWLINE = 'newline'
+const CORRECT = true
+const INCORRECT = false
+
 let sentences = [
-    'asdf asdf asdf asdf asdf asdfasdf',
-    // 'asdf ASDF ASDF ASDF ASDFASDF ASDF',
-    // 'asdfasdfasdfasdf',
-    // 'ten ate neite ate nee enet ite ate inet ent eate',
-    // 'Too ato too nOt enot one totA not anot tOO aNot',
-    // 'oat itain oat tain nate eate tea anne inant nean',
-    // 'itant eate anot eat nato inate eat anot tain eat',
-    // 'nee ene ate ite tent tiet ent ine ene ete ene ate',
+    'ten ate neite ate nee enet ite ate inet ent eate',
+    'Too ato too nOt enot one totA not anot tOO aNot',
+    'oat itain oat tain nate eate tea anne inant nean',
+    'itant eate anot eat nato inate eat anot tain eat',
+    'nee ene ate ite tent tiet ent ine ene ete ene ate',
 ]
 
 let state
 
-function setupState() {
-    state = {
-        row: 0,
-        col: 0,
-        accurateWord: true,
-        correct: 0,
-        incorrect: 0,
-        missedChar: 0,
-        startTime: null,
-        minutes: undefined,
-    }
-}
-
-function setupGame() {
-    $('#target-letter').text(sentences[state.row][state.col])
-    $('#sentence').text(sentences[0])
-    $('#prompt-container').slideDown()
-
-    // Handle key highlighting
-    $(document).on('keypress', async function (e) {
-        if (e.which < 32 || e.which > 126) {
-            return
-        }
-        let success = userTyped(String.fromCharCode(e.which))
-
-        let gly
-        if (success !== 'newline') {
-            gly = {
-                true: 'glyphicon-ok',
-                false: 'glyphicon-remove',
-                'space': 'glyphicon-minus',
-            } [success]
-            $('#feedback').append($('<span class="glyphicon ' + gly + '"></span>'))
-        }
-
-        $('#' + e.which).css(
-            'background-color',
-            success ? 'lightgreen' : 'red'
-        )
-        await sleep(125)
-        $('#' + e.which).css('background-color', '#f5f5f5')
-    })
-}
-
-$(document).ready(async function () {
-    setupState()
-    setupGame()
+$(document).ready(function () {
 
     $(document).keydown(function (e) {
         if (state.startTime === null) {
@@ -76,6 +32,13 @@ $(document).ready(async function () {
         }
     })
 
+    $('#load-btn').focus()
+    $('#load-btn').click(function () {
+        $('#greeter').hide()
+        setupState()
+        setupGame()
+    })
+
     $('#reset-btn').click(function () {
         $('#end-game').hide()
         setupState()
@@ -83,17 +46,67 @@ $(document).ready(async function () {
     })
 })
 
+function setupState() {
+    state = {
+        accurateWord: true,
+        correct: 0,
+        incorrect: 0,
+
+        row: 0,
+        col: 0,
+        missedChar: 0,
+        startTime: null,
+        minutes: null,
+    }
+}
+
+function setupGame() {
+    $('#target-letter').text(sentences[state.row][state.col])
+    $('#sentence').text(sentences[0])
+    $('#prompt-container').slideDown()
+
+    $(document).on('keypress', async function (e) {
+        if (e.which < 32 || e.which > 126) {
+            return
+        }
+
+        let result = userTyped(String.fromCharCode(e.which))
+        let gly
+
+        if (result !== NEWLINE) {
+            switch (result) {
+                case SPACE:
+                    gly = 'glyphicon-minus'
+                    break
+                case CORRECT:
+                    gly = 'glyphicon-ok'
+                    break
+                case INCORRECT:
+                    gly = 'glyphicon-remove'
+                    break
+            }
+            $('#feedback').append($('<span class="glyphicon ' + gly + '"></span>'))
+        }
+
+        $('#' + e.which).css(
+            'background-color',
+            result ? 'lightgreen' : 'red'
+        )
+        await sleep(125)
+        $('#' + e.which).css('background-color', '#f5f5f5')
+    })
+}
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function userTyped(key) {
-    console.log(state, sentences[state.row][state.col])
     let sentence = sentences[state.row]
     let char = sentence[state.col++]
 
     let returnVal
-    // This purposely does not check spaces (I'm being nice)
+    // This purposely does not check for incorrect spaces (I'm being nice)
     if (char === ' ') {
         if (state.accurateWord) {
             state.correct++
@@ -101,54 +114,60 @@ function userTyped(key) {
             state.incorrect++
         }
         state.accurateWord = true
-        shiftYellow()
-        returnVal = 'space'
+        returnVal = SPACE
     } else if (key === char) {
-        shiftYellow()
-        returnVal = true
+        returnVal = CORRECT
     } else {
-        shiftYellow()
         state.accurateWord = false
         state.missedChar++
-        returnVal = false
+        returnVal = INCORRECT
     }
+
+    shiftYellow()
 
     if (state.col >= sentence.length) {
         shiftYellow(reset = true)
+
         sentence = sentences[++state.row]
-        $('#sentence').text(sentence)
         state.col = 0
+
+        $('#sentence').text(sentence)
+        $('#feedback').html('')
+
         if (state.accurateWord) {
             state.correct++
         } else {
             state.incorrect++
         }
         state.accurateWord = true
-        $('#feedback').html('')
-        returnVal = 'newline'
+        returnVal = NEWLINE
     }
+
     if (state.row >= sentences.length) {
         $(document).off('keypress')
         endGame()
-        return returnVal
+    } else {
+        $('#target-letter').text(sentence[state.col].replace(' ', '<space>'))
     }
-    $('#target-letter').text(sentence[state.col].replace(' ', '\<space\>')) //does this need to change
 
     return returnVal
 }
 
 function endGame() {
     state.minutes = (Date.now() - state.startTime) / 1000 / 60
-    $('#prompt-container').slideUp(200)
+
     $('#score').html(
-        '<p>Your final score: ' +
+        '<h4>Your final score: ' +
         Math.round((sentences.join(' ')).split(' ').length / state.minutes - 2 * state.missedChar) +
-        ' (words per minute)</p>' +
-        '<p>Number of correct words: ' + state.correct + '. Incorrect: ' + state.incorrect +
-        '. Character mistakes: ' + state.missedChar + '</p>'
+        ' (words per minute)</h4>' +
+        '<h4>Number of correct words: ' + state.correct + '. Incorrect: ' + state.incorrect +
+        '. Character mistakes: ' + state.missedChar + '</h4>'
     )
+
+    $('#prompt-container').slideUp(200)
     $('#end-game').slideDown()
     $('#reset-btn').focus()
+
     $(document).on('keypress', async function (e) {
         if (e.which < 32 || e.which > 126) {
             return
@@ -175,6 +194,7 @@ function endGame() {
 }
 
 async function makeRainbow() {
+    await sleep(100)
     for (let i = 0; i < 200; i++) {
         console.log('test')
         await sleep(15)
