aa221da9f0ed1b4be0572ef092a2f0d6ae71d1e1
Noah May <noahmouse2011@gmail.com>
2019-07-08 10:03:56 -0600

    Completed ES6 Drills lab

---
+++ es6-drills/app.js
@@ -0,0 +1,44 @@
+function favMovie(movie = 'The Room', name = 'the world') {
+    console.log(`I am ${name} and my favorite movie is "${movie}"`)
+}
+
+favMovie()
+favMovie('Sherlock Holmes: A Game of Shadows', 'Bartholomew')
+
+// Doesn't work as I expected...
+favMovie(name = 'a child', movie = 'Bob the Builder')
+
+let arrowMovie = (name = 'the world', movie = 'The Room') => console.log(`I am ${name} and my favorite movie is "${movie}"`)
+
+arrowMovie('a child', 'Bob the Builder')
+
+
+let getFirstName1 = (name) => {
+    return name.split(' ')[0]
+}
+
+let getFirstName2 = name => name.split(' ')[0]
+
+let stats = (x, y) => ({
+    exponentiation: x ** y,
+    product: x * y,
+})
+
+let result = stats(2, 3)
+console.log(`${result.exponentiation} ${result.product}`)
+
+function seriously(name, location, favFood) {
+    console.log(`Wait, you eat ${favFood} at ${location}? Seriously, ${name}!`)
+}
+
+let arrrrrgs = ['Bartholomew', 'Subway', 'McChickens']
+seriously(...arrrrrgs); // Why does this semicolon have to be necessary?!?!
+
+(function (name) {
+    let chars = [...name]
+    console.group('Spreading strings:')
+    for (let i in chars) {
+        console.log(i + ':', chars[i])
+    }
+    console.groupEnd()
+})('Noah May')
