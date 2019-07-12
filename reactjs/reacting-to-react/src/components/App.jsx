7dba12770296fd77500ec63a273bb64fb0e91dc8
Noah May <noahmouse2011@gmail.com>
2019-07-12 11:46:51 -0600

    Finished Reacting to Reactjs lab

--- reactjs/reacting-to-react/src/components/App.jsx
+++ reactjs/reacting-to-react/src/components/App.jsx
@@ -1,18 +1,60 @@
-// App.jsx
-import React from 'react'
+import React, { Component } from 'react'
 
-// Old, still renders exactly like this
 // let App = (props) => {
 //     return (
 //         <h1>{props.message}</h1>
 //     )
 // }
 
-let App = (props) => {
-    alert('Never pulls up')
-    return (
-        <h1>{props.message + 'your face'}</h1>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.props = props
+        this.state = {
+            text: props.message,
+            inputValue: null,
+            hasLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            hasLoaded: true,
+        })
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            inputValue: e.target.value,
+        })
+    }
+
+    handleLoadingClick = (e) => {
+        this.setState({
+            hasLoaded: !this.state.hasLoaded,
+        })
+    }
+
+    render() {
+        console.log(this.props)
+        if (this.state.hasLoaded) {
+            return (
+                <>
+                    <h1>{this.props.message} {this.state.text}</h1>
+                    <input type="text" onChange={this.handleInputChange} placeholder="type here" />
+                    <p>Input appears here: {this.state.inputValue}</p>
+                    <button onClick={this.handleLoadingClick}>"Unload"</button>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <h1>Loading...</h1>
+                    <button onClick={this.handleLoadingClick}>Click to magically load faster!</button>
+                </>
+            )
+        }
+    }
 }
 
 export default App
--- reactjs/reacting-to-react/src/index.jsx
+++ reactjs/reacting-to-react/src/index.jsx
@@ -1,6 +1,5 @@
-// index.jsx
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
-ReactDOM.render(<App message={"testing..."} />, document.getElementById('root'))
+ReactDOM.render(<App message={"Hello world!"} />, document.getElementById('root'))
