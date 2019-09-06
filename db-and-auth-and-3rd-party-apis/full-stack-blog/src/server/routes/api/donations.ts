6035742b943d6115c1f3422499fbcbb94d49c656
Noah May <noahmouse2011@gmail.com>
2019-09-05 22:46:43 -0600

    Moved stripe charges to its own util file

--- db-and-auth-and-3rd-party-apis/full-stack-blog/src/server/routes/api/donations.ts
+++ db-and-auth-and-3rd-party-apis/full-stack-blog/src/server/routes/api/donations.ts
@@ -1,18 +1,16 @@
 import {Router} from 'express'
-import * as stripeLoader from 'stripe'
+
+import {stripeCharge} from '../../utils/apis/stripe'
 
 const router = Router()
 
-const stripe = new stripeLoader(process.env.STRIPE_SK)
-
 router.post('/', async (req, res) => {
     let {tokenid, amount, name}: {tokenid: string, amount: string, name: string} = req.body
-    let donation = await stripe.charges.create({
-        currency: 'usd',
-        amount: parseInt(amount)*100,
-        source: tokenid,
-        description: 'Donation by: '+name,
-    })
+    let donation = await stripeCharge(
+        tokenid,
+        parseInt(amount),
+        'Donation by: '+name,
+    )
     if (!donation) {
         res.status(400).json('Failed to create charge')
     }
---
+++ db-and-auth-and-3rd-party-apis/full-stack-blog/src/server/utils/apis/stripe.ts
@@ -0,0 +1,12 @@
+import * as stripeLoader from 'stripe'
+
+const stripe = new stripeLoader(process.env.STRIPE_SK)
+
+export const stripeCharge = (tokenid: string, amountUSD: number, description: string) => {
+    return stripe.charges.create({
+        source: tokenid,
+        amount: amountUSD * 100,
+        currency: 'usd',
+        description,
+    })
+}
