9db4ed716e487ed54d3266b0655e20ce4e357d86
Noah May <noahmouse2011@gmail.com>
2019-09-11 13:50:02 -0600

    Finished simple lab - first mobile app

--- react-native/first-lab/App.tsx
+++ react-native/first-lab/App.tsx
@@ -5,8 +5,12 @@ import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
+        <View style={{ flex: 1, backgroundColor: 'red', padding: 20 }}>
+            <View style={{ flex: 1, backgroundColor: 'black', padding: 20 }}>
+                <View style={styles.container}>
+                    <Text style={styles.text}>#crushingit</Text>
+                </View>
+            </View>
         </View>
     )
 }
@@ -14,8 +18,14 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        color: 'red',
+        backgroundColor: 'lightgreen',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    text: {
+        fontSize: 40,
+        color: 'white',
+        backgroundColor: 'red',
     },
 })
